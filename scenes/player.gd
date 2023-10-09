84af857adab13681636cdf3d681451465132b7ac
George Kanunnikov <dcel567@gmail.com>
2023-10-09 13:18:27 +0300

    enemies and shooting progression (but still nothing de facto)

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
+signal player_shoot()
+
 const SPEED = 400
+var can_shoot : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$shoot_timer.wait_time = 0.5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,5 +19,12 @@ func _physics_process(delta):
 	velocity = direction * SPEED
 	move_and_slide()
 	
-	if Input.is_action_pressed("primary_action"):
-		print("bullet")
+	if Input.is_action_pressed("primary_action") and can_shoot:
+		player_shoot.emit()
+		can_shoot = false
+		$shoot_timer.start()
+
+
+func _on_shoot_timer_timeout():
+	can_shoot = true
+
