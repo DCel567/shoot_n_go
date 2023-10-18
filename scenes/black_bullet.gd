58a5d57a2a3e967e9cea3c28f9b0158ede60d8a0
George Kanunnikov <dcel567@gmail.com>
2023-10-18 13:30:11 +0300

    Particles fixed. Bullets collide only once. TODO: fixe collisions if player is shooting near obstacle

--- scenes/black_bullet.gd
+++ scenes/black_bullet.gd
@@ -3,10 +3,20 @@ extends RigidBody2D
 @export var speed = 10
 var direction = Vector2.RIGHT
 
+func _ready():
+	$CollisionParticlesWhite.wait_time = $GPUParticles2D.lifetime
+
 func _physics_process(delta):
 	position += speed * direction
 
 
 func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	#$GPUParticles2D.emitting = true
+	$GPUParticles2D.emitting = true
+	$YellowBullet.visible = false
+	$CollisionShape2D.set_deferred("disabled", true) 
+	$CollisionParticlesWhite.start()
+
+
+func _on_collision_particles_white_timeout():
 	queue_free()
+	
