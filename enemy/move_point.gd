80bad0af85bcf6bd11254bbfb4540f26ea2601e5
George Kanunnikov <dcel567@gmail.com>
2023-12-08 13:15:34 +0300

    enemies now support a lot of move points and it is possible to pick move_point stop time

--- enemy/move_point.gd
+++ enemy/move_point.gd
@@ -1,6 +1,10 @@
 extends Node2D
 class_name MovePoint
 
+@export var before_move: float = 2.0
 
-func get_coords():
+func get_coords() -> Vector2:
 	return $coords.global_position
+	
+func get_timer() -> float:
+	return before_move
--- enemy/moveto_module.gd
+++ enemy/moveto_module.gd
@@ -2,6 +2,7 @@ extends Node
 class_name MovetoModule
 
 var positions: Array
+var before_move_timers: Array
 var curr_pos: int = 0
 var have_positions: bool = false
 var positions_amount: int = 0
@@ -18,21 +19,33 @@ func _ready():
 		have_positions = true
 		
 		positions.push_back(child.get_coords())
+		before_move_timers.push_back(child.get_timer())
 		get_tree().queue_delete(child)
 		positions_amount += 1
 
 
 func _process(_delta):
 	if moving:
-		var direction = (Vector2(positions[0])-$"..".global_position).normalized()
-		$"..".velocity = direction * $"..".SPEED
-		$"..".move_and_slide()
+		move_to(positions[curr_pos])
 	
 
 func start_moving():
-	moving = true
+	if have_positions:
+		moving = true
 	
-
+func move_to(glob_pos: Vector2):
+	var diff: Vector2 = Vector2(glob_pos)-$"..".global_position
+	
+	if diff.length() < 3:
+		moving = false
+		await get_tree().create_timer(before_move_timers[curr_pos]).timeout
+		curr_pos = (curr_pos + 1) % positions_amount
+		moving = true
+		return
+		
+	var direction = (diff).normalized()
+	$"..".velocity = direction * $"..".SPEED
+	$"..".move_and_slide()
 
 
 
